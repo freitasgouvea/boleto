bac8bb0bf14a555de583980f5747b9b965e15183
Flavio Gouvea <flavio@4cadia.com>
2020-04-23 23:52:04 -0300

    blockchain.js

--- js/blockchain.js
+++ js/blockchain.js
@@ -1,6 +1,9 @@
-var contract;
-var provider;
-var contractAddress;
+var status;
+
+var contractAddress = "0xE1E8035ae15EcD8D5b4593CAF22Eff90d95A209D"; //add
+var provider = ethers.getDefaultProvider('rinkeby'); //main
+var contract = new ethers.Contract(contractAddress, abiContrato, provider);
+
 
 function canPerformTransaction() {
     if (signerAddress.length === 42) {
@@ -41,4 +44,21 @@ async function loadBoletoSC() {
     } catch (e) {
         console.error("loadBoletoSC", e.message);
         throw e;
-   
+    }
+}
+
+async function obtemBoletoHash() {
+    let frm = document.boletoForm
+    try {
+        if (contract) {
+            console.log('obtemBoletoHash', 'iniciando busca', frm.boleto.value)
+            let detalhes = await contract.verBoleto(frm.boleto.value)
+            document.getElementById("viewBoleto").style.display = "block"
+            $("#viewBoleto").html(detalhes);
+            document.getElementById("boletoForm").style.display = "none"
+        }
+    } catch (err) {
+        console.error('obtemBoletooHash', err)
+        alert("Não foi possível encontrar este boleto, tente novamente.")
+    }
+}
