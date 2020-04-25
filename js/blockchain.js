3e354f10b57b76c88757c52c9f218394525c50a7
Flavio Gouvea <flavio@4cadia.com>
2020-04-25 16:48:16 -0300

    metamask

--- js/blockchain.js
+++ js/blockchain.js
@@ -1,4 +1,4 @@
-var abiContrato = [
+let abiContrato = [
 	{
 		"inputs": [
 			{
@@ -303,6 +303,10 @@ let provider = new ethers.providers.InfuraProvider('rinkeby', 'd7af4ca348a2460aa
 let contractAddress = "0xAbaC6e1dDcE1c2B4dafBD6E6a1Adb3096Fe2Cb61";
 let contract = new ethers.Contract(contractAddress, abiContrato, provider);
 
+let web3Provider = new ethers.providers.Web3Provider(web3.currentProvider);
+const signer = provider.getSigner();
+console.log(web3Provider, signer);
+
 function timestampToDate(unixtime) {
 
 	var u = new Date(unixtime * 1000);
@@ -313,6 +317,11 @@ function timestampToDate(unixtime) {
 		' - ' + ('0' + u.getUTCHours()).slice(-2) +
 		':' + ('0' + u.getUTCMinutes()).slice(-2) +
 		':' + ('0' + u.getUTCSeconds()).slice(-2)
+}
+
+async function getSigner() {
+	let signer = await web3Provider.getSigner();
+	console.log(signer);
 }
 
 async function obtemBoletoHash() {
@@ -334,7 +343,7 @@ async function obtemBoletoHash() {
 			document.getElementById("vencimentoBoleto").innerHTML = timestampToDate(view[3]);
 			document.getElementById("valorAtualizadoBoleto").innerHTML = view[5] / 1000000000000000000;
 			document.getElementById("dataLimiteBoleto").innerHTML = timestampToDate(view[4]);
-			document.getElementById("linkBoleto").innerHTML = "https://boletom.com.br/" + details[0];
+			document.getElementById("linkBoleto").innerHTML = "https://boleto.com.br/" + details[0];
 			if (details[2] === false && details[3] === false) {
 				let recipe = await contract.reciboBoleto(frm.boleto.value)
 				console.log(recipe)
